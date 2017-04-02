b1bfcdd9d9a3d2b6be63647eef772f4b56921286
Lillyan <ldp54@cornell.edu>
2017-04-02 08:43:06 -0400

    heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 8080));
 
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, '/client')));
 
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
